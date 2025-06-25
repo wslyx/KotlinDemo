43361dcb789722ae6e1d4ec5ba78386ae7f74c9c
wslyx <yiyayo8077@outlook.com>
2025-06-25 22:21:27 +0800

    feat(ReflectGetRetType): 添加获取类型名称的功能
    - 新增 getTypeName 函数，使用 reified 类型参数
    - 在 main函数中调用 getTypeName 函数并打印结果

--- src/main/kotlin/ReflectGetRetType.kt
+++ src/main/kotlin/ReflectGetRetType.kt
@@ -1,4 +1,11 @@
 package top.superyaxi
 
-class ReflectGetRetType {
+inline fun <reified T> getTypeName(): String {
+    return T::class.java.name // 可以直接访问类型信息
+}
+
+fun main() {
+    // 使用
+    val typeName = getTypeName<String>() // 返回 "java.lang.String"
+    println("\n\t"+typeName)
 }
