ae37f741f62c0df1d4d9de87d8edd947e876afe8
superyaxi <yiyayo8077@outlook.com>
2025-07-01 15:33:12 +0800

    refactor(EnumKlass): 优化枚举类代码结构和输出格式
    - 在 Color2 枚举类中添加 constructor 关键字，明确表示构造函数
    - 添加注释说明枚举类默认包含的属性和自定义属性
    - 修改 main 函数中的输出格式，增加更多枚举类属性的展示- 添加对枚举类实例索引 (ordinal) 和名称 (name) 的输出

--- src/main/kotlin/klassAndObject/EnumKlass.kt
+++ src/main/kotlin/klassAndObject/EnumKlass.kt
@@ -4,7 +4,9 @@ enum class Color {
     RED,BLACK,BLUE,GREEN,WHITE
 }
 
-enum class Color2(val rgb: Int) {
+// 默认包含属性 ordinal(索引) name(实例名称)
+// 其他自定义属性像data class一样，在构造函数括号中定义
+enum class Color2 constructor (val rgb: Int) {
     RED(0xFF0000),
     GREEN(0x00FF00),
     BLUE(0x0000FF)
@@ -13,16 +15,20 @@ enum class Color2(val rgb: Int) {
 @OptIn(ExperimentalStdlibApi::class)
 fun main() {
     println("Enum Demo 01")
-    println(Color.RED)
-    println(Color.BLUE)
-    println(Color.GREEN)
-    println(Color.BLACK)
-    println(Color.WHITE)
+    println("Color.RED: ${Color.RED}")
+    println("Color.BLUE: ${Color.BLUE}")
+    println("Color.GREEN: ${Color.GREEN}")
+    println("Color.BLACK: ${Color.BLACK}")
+    println("Color.WHITE: ${Color.WHITE}")
     println("Enum Demo 02")
-    println(Color2.RED)
-    println(Color2.GREEN)
-    println(Color2.BLUE)
-    println(Color2.RED.rgb.toHexString())
-    println(Color2.GREEN.rgb.toHexString())
-    println(Color2.BLUE.rgb.toHexString())
+    println("Color2.RED: ${Color2.RED}")
+    println("Color2.GREEN: ${Color2.GREEN}")
+    println("Color2.BLUE: ${Color2.BLUE}")
+    println("Color2.RED.rgb: ${Color2.RED.rgb.toHexString()}")
+    println("Color2.GREEN.rgb: ${Color2.GREEN.rgb.toHexString()}")
+    println("Color2.BLUE.rgb: ${Color2.BLUE.rgb.toHexString()}")
+    // 索引
+    println("Color2.RED的ordinal索引:${Color2.RED.ordinal}")
+    // name
+    println("Color2.RED的name:${Color2.RED.name}")
 }
