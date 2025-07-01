ff943a52a028a65716b5bbcd45e51c7264aeb6cf
superyaxi <yiyayo8077@outlook.com>
2025-07-01 11:07:43 +0800

    test(DataKlass): 测试数据类的特性
    - 添加了 toString() 方法的测试
    - 添加了 copy() 方法的测试
    - 添加了 equals() 方法的测试
    - 添加了 hashCode() 方法的测试
    - 添加了 componentN() 方法的测试
    - 添加了关于 Kotlin 中 == 与 equals() 的说明

--- src/main/kotlin/klassAndObject/DataKlass.kt
+++ src/main/kotlin/klassAndObject/DataKlass.kt
@@ -15,5 +15,27 @@ fun main() {
     dataKlass.a = 1000
     dataKlass.b = "NEW STRING DEMO"
     dataKlass.c = true
+    // toString() 测试
     println(dataKlass)
+    // 复制，同 Java Clone
+    val copyDataKlass = dataKlass.copy()
+    // equals() 测试
+    println("equals测试:" + copyDataKlass.equals(dataKlass))
+    // hashCode() 测试
+    println("hashCode测试{ copy:" + copyDataKlass.hashCode() + " dataKlass:" + dataKlass.hashCode() + " }")
+    // 修改copy后的属性
+    copyDataKlass.a = 1001
+    println("修改copy属性后的数据类")
+    // equals() 测试
+    println("equals测试:" + copyDataKlass.equals(dataKlass))
+    // hashCode() 测试
+    println("hashCode测试{ copy:" + copyDataKlass.hashCode() + " dataKlass:" + dataKlass.hashCode() + " }")
+    // componentN() 测试
+    println("component1测试:" + copyDataKlass.component1())
+    println("component2测试:" + copyDataKlass.component2())
+    println("component3测试:" + copyDataKlass.component3())
 }
+
+/*
+kotlin的==与equals()说明
+ */
