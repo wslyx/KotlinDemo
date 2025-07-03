c002230a1405b9ccf7c794a2c3d61a955e0dbdc4
wslyx <yiyayo8077@outlook.com>
2025-07-03 12:35:51 +0800

    feat(coroutineDemo): 添加协程上下文中的扩展函数和 Lambda - 在 CoroutineContextDemo.kt - 定义了 myAction、myCoroutineFunction 和 myAction2三个新的协程块 - 展示了如何在协程上下文中使用扩展函数和 Lambda - 增加了 delay 函数的使用，模拟异步操作

--- src/main/kotlin/coroutineDemo/CoroutineContextDemo.kt
+++ src/main/kotlin/coroutineDemo/CoroutineContextDemo.kt
@@ -2,6 +2,7 @@ package top.superyaxi.coroutineDemo
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -17,5 +18,22 @@ fun main() {
         launch(Dispatchers.Unconfined) {
             println("Unconfined协程上下文:" + this.coroutineContext)
         }
+        launch(Dispatchers.Default, block = myAction)
+        launch(Dispatchers.Default, block = CoroutineScope::myCoroutineFunction)
+        launch(Dispatchers.Default, block = myAction2)
     }
 }
+
+val myAction: suspend CoroutineScope.() -> Unit = {
+    delay(1000)
+    println("Executed in ${this.coroutineContext}")
+}
+
+suspend fun CoroutineScope.myCoroutineFunction() {
+    // 在 CoroutineScope 上下文中执行挂起操作
+    delay(1000)
+    println("Executed in ${this.coroutineContext}")
+}
+
+val myAction2: suspend CoroutineScope.() -> Unit = CoroutineScope::myCoroutineFunction
+
