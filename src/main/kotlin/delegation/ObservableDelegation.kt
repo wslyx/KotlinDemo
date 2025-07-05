6b93d68d4cab2da20be96593ddf96687da987a9d
wslyx <yiyayo8077@outlook.com>
2025-07-05 18:28:44 +0800

    refactor(delegation): 优化 ObservableDelegation.kt 文件
    - 移除不必要的 name = new赋值语句，避免无限递归
    - 添加 user.name 属性变化后的打印语句，用于验证功能
    - 优化代码注释，提高可读性

--- src/main/kotlin/delegation/ObservableDelegation.kt
+++ src/main/kotlin/delegation/ObservableDelegation.kt
@@ -11,6 +11,7 @@ class User {
     // Delegates.observable() 函数接受两个参数: 第一个是初始化值, 第二个是属性值变化事件的响应器(handler)。
     // 在属性赋值后会执行事件的响应器(handler)，它有三个参数：被赋值的属性、旧值和新值
     var name: String by Delegates.observable("初始值") {
+        // 此处只需要监听属性 name 的变化，不需要name = new，业务代理机制会自动更新（手动赋值导致再次触发相同的处理器，形成无限递归调用栈）
         prop, old, new -> nameChange(old, new);println("多行测试2")
     }
     var age: Int by Delegates.observable(0) {
@@ -27,6 +28,7 @@ fun main(args: Array<String>) {
     val user = User()
     user.name = "第一次赋值"
     user.name = "第二次赋值"
+    println("user.name = ${user.name}")
     user.age = 18
     user.age = 19
     user.height = 1.8f
