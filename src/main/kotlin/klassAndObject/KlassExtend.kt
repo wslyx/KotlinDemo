cd75db855bb7faa9c71c4faca261daa1c5e28234
wslyx <yiyayo8077@outlook.com>
2025-07-15 20:15:04 +0800

    feat(klassAndObject): 添加用户类扩展函数和反射调用示例
    - 新增 UserExternFun 类型别名，用于定义用户类的扩展函数
    - 实现 helloExtend 扩展函数，可在用户实例上调用
    - 在 main 函数中演示反射获取并调用用户类的成员函数

--- src/main/kotlin/klassAndObject/KlassExtend.kt
+++ src/main/kotlin/klassAndObject/KlassExtend.kt
@@ -11,8 +11,21 @@ fun User.print(){
     println("用户名是 $name")
 }
 
+// 不属于 User 本身，但是可以像反射获取到的成员函数一样调用 User 的实例
+typealias UserExternFun = User.() -> Unit
+
+fun helloExtend(): UserExternFun = {
+    println("hello $name")
+}
+
 fun main(arg:Array<String>){
     val user = User("Runoob")
     user.print()
     println("类对象输出:$user")
+
+    //反射获取print函数
+    val print = User::print
+    print.invoke(user)
+
+    helloExtend().invoke(user)
 }
